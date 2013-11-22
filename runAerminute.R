ee072bfe9928ebed294323a40b5fb5ea16646758
YoJimboDurant <Hzd3@D303553.cdc.gov>
2013-11-22 17:08:50 -0500

    added runAerminute.R function to run Aerminute

---
+++ runAerminute.R
@@ -0,0 +1,13 @@
+# runs aerminute
+
+runAerminute = function(startYear, stopYear, filePrefix="AM"){
+  years <- seq(startYear, stopYear, by=1)
+  for(i in 1:length(years)){
+    aerminute <- paste(getwd(),"/aerminute_v11325/aerminute_11325.exe", sep="")
+    setwd(paste(".\\", years[i],sep=""))
+    shell(aerminute, input=paste(filePrefix, years[i],".inp", sep=""))
+    setwd("..")
+  }
+  
+  
+}
